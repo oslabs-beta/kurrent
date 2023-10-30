4d9690112cbf5fb18838fb1c03348e6b2657310c
Swarna2072 <swarna2072@gmail.com>
2023-10-30 09:07:06 -0400

    updated db.js

--- db.js
+++ db.js
@@ -1,5 +1,12 @@
 // db.js
 const { Pool } = require('pg');
+// I used this query to create table
+// CREATE TABLE users (
+//     user_id serial PRIMARY KEY,
+//     username VARCHAR(255) NOT NULL,
+//     password VARCHAR(255) NOT NULL,
+//     email VARCHAR(255)
+// );
 
 const pool = new Pool({
   connectionString: 'postgres://owpkxuij:RCy03blF6Cmvz4cUiYQFEUZujP8ublao@castor.db.elephantsql.com/owpkxuij',
