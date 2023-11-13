cb48d9abbf4a30cac158c970314a5548602c6a12
jensenrs <jensenrschmidt@gmail.com>
2023-11-13 10:23:41 -0500

    minor changes to unused code

--- client/containers/Login.jsx
+++ client/containers/Login.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import '../scss/login.scss';
 
